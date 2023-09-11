75d06e23ac912176101f5c891abf397093c06326
therealhazem <therealhazemelgindy@gmail.com>
2023-09-11 09:57:35 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - Entry Point
  *
  * Description: print a qoute using write function
- * 	ssize_t write(int fd,const void *buf,size_t count)
+ *	ssize_t write(int fd,const void *buf,size_t count)
  *
  * Return: 1 (error).
  */
