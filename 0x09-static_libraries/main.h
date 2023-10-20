c50672c93531242618be8e2d09786f3bc2ee56be
therealhazem <140704105+therealhazem@users.noreply.github.com>
2023-10-20 17:03:17 +0300

    static real

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,10 +1,8 @@
-#ifndef MAIN_H
-#define MAIN_H
-/**
- * File: main
- *
- *Desc: header file
- */
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+/*a list that was given from the task 0 */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
